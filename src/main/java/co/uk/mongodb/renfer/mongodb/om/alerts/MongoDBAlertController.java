c391d5dd9ffc144c176d9390a0acf2ade261439d
codedearta <sepp.renfer@gmail.com>
2020-04-08 17:18:44 +0200

    added java client error handler

--- src/main/java/co/uk/mongodb/renfer/mongodb/om/alerts/MongoDBAlertController.java
+++ src/main/java/co/uk/mongodb/renfer/mongodb/om/alerts/MongoDBAlertController.java
@@ -1,15 +1,18 @@
 package co.uk.mongodb.renfer.mongodb.om.alerts;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
 @RestController
 public class MongoDBAlertController {
 
-//    @PostMapping("/javaClientAlert")
-//    public void javaClientAlert(@RequestBody AlertsResponse notification) {
-////        MongoDBClusterHealthIndicator.IS_HEALTHY = notification.getErrorCode();
-//    }
+    @PostMapping("/javaClientAlert")
+    public void javaClientAlert(@RequestBody Map<String, Object> alert) {
+        MongoDBJavaClientHealthIndicator.sendAlert(alert);
+    }
 
     @PostMapping("/omAlert")
     public void omAlert(@RequestBody Map<String, Object> alert) {
---
+++ src/main/java/co/uk/mongodb/renfer/mongodb/om/alerts/MongoDBJavaClientHealthIndicator.java
@@ -0,0 +1,36 @@
+package co.uk.mongodb.renfer.mongodb.om.alerts;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MongoDBJavaClientHealthIndicator implements HealthIndicator {
+
+    private static Map<String, Object> latestAlert = new HashMap<>();
+
+    public static void sendAlert(Map<String, Object> alert) {
+        latestAlert = alert;
+    }
+
+    @Override
+    public Health health() {
+        final String status = (String) latestAlert.getOrDefault("status", "");
+
+        if(status.equals("")){
+            return Health.up().build();
+        }else{
+            AlertStatus alertStatus = AlertStatus.valueOf(status);
+            switch(alertStatus) {
+                case TRACKING:
+                case OPEN:
+                    return Health.down().withDetails(latestAlert).build();
+                case CLOSED:
+                case CANCELLED:
+                default:
+                    return Health.up().withDetails(latestAlert).build();
+            }
+        }
+    }
+}
